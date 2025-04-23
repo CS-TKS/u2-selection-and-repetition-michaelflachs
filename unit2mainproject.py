c99e96b089170821f24ae7c680ae96253614fe4a
michaelflachs <michael.flachs@thekaustschool.org>
2025-04-23 12:58:06 +0300

    Unit 2 Progress
    Starting

---
+++ unit2mainproject.py
@@ -0,0 +1,22 @@
+
+#input section talking about
+
+import time
+print("Welcome to this Minigame!👋")
+print("Your objective will be to cool down the planet by eliminating objects which you think harm the planet 🌍")
+print("Based off your understanding, if you think you are confident type yes, if you are not type no")
+
+#processing section
+client = input("So what will it be? (yes/no)")
+
+#game section if yes confident there will be more objects
+if client == "yes":
+    difficulty = "hard"
+    objects = ["Trash bag", "Car", "Plant", "Plane", "Factory", "Tree", "Animal Cow", "Forest"]
+    round = 5
+elif client == "no":
+    difficulty = "easy"
+    objects = ["Trash", "Ship", "Bus", "Plant", "Humans", "Solar Panel"]
+    round = 3
+
+usefulobject = ["Plant", "Solar Panel", "Tree", "Forest"]
